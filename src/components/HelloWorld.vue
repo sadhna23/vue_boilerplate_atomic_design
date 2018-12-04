b85d9c4d96572688171b597f94bf346ed43d1ff1
Vinod Kumar <vinod.kumar1@zensar.com>
2018-12-04 16:50:13 +0530

    Foundation Steps

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <AppButton label="Search" shape="circle"/>
+    <Input placeholder="Enter your name"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AppButton from './atoms/AppButton'
+import Input from './atoms/AppInput'
+
+export default {
+  name: "HelloWorld",
+
+  components:{
+    AppButton,
+    Input
+  }
+};
+</script>
---
+++ src/components/atoms/AppButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button :class="shape">
+        {{label}}
+    </button>
+</template>
+
+
+
+<script>
+export default {
+    name: 'AppButton',
+    props:{
+        label: String,
+        shape:{
+            type: String,
+            default: 'square'
+        }
+    }
+}
+</script>
+
